e83c662c733c165a6712b39eb68d1d387bba622f
Kevin Soo <kevinsoo2005@hotmail.com>
2024-06-25 01:25:00 -0700

    Testing Randomize Age

--- Character_Generator/src/Soo/CharacterData.java
+++ Character_Generator/src/Soo/CharacterData.java
@@ -1,6 +1,9 @@
 package Soo;
 
+import java.util.Random;
+
 public class CharacterData {
+	private int age;
 	private String name;
 	private String race;
 	private String Gender;
@@ -10,5 +13,111 @@ public class CharacterData {
 	private String weapon;
 	private String traits;
 	private String affinity;
-	private int age;
+	
+	public CharacterData() {};
+	
+	public CharacterData(int age, String name, String race, String gender, String background, String occupation,
+			String characterClass, String weapon, String traits, String affinity) {
+		super();
+		this.age = age;
+		this.name = name;
+		this.race = race;
+		Gender = gender;
+		Background = background;
+		this.occupation = occupation;
+		this.characterClass = characterClass;
+		this.weapon = weapon;
+		this.traits = traits;
+		this.affinity = affinity;
+	}
+
+	public void randomizeAge(int minAge, int maxAge) {
+		if(minAge < 0 || maxAge < 0 || minAge > maxAge) {
+			throw new IllegalArgumentException("Invalid age range");
+		}
+		Random rand = new Random();
+		this.age = rand.nextInt((maxAge - minAge)+1)+minAge;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRace() {
+		return race;
+	}
+
+	public void setRace(String race) {
+		this.race = race;
+	}
+
+	public String getGender() {
+		return Gender;
+	}
+
+	public void setGender(String gender) {
+		Gender = gender;
+	}
+
+	public String getBackground() {
+		return Background;
+	}
+
+	public void setBackground(String background) {
+		Background = background;
+	}
+
+	public String getOccupation() {
+		return occupation;
+	}
+
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+
+	public String getCharacterClass() {
+		return characterClass;
+	}
+
+	public void setCharacterClass(String characterClass) {
+		this.characterClass = characterClass;
+	}
+
+	public String getWeapon() {
+		return weapon;
+	}
+
+	public void setWeapon(String weapon) {
+		this.weapon = weapon;
+	}
+
+	public String getTraits() {
+		return traits;
+	}
+
+	public void setTraits(String traits) {
+		this.traits = traits;
+	}
+
+	public String getAffinity() {
+		return affinity;
+	}
+
+	public void setAffinity(String affinity) {
+		this.affinity = affinity;
+	}
+
+	
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
--- Character_Generator/src/Soo/CharacterGen.java
+++ Character_Generator/src/Soo/CharacterGen.java
@@ -4,7 +4,23 @@ public class CharacterGen {
 
 	public static void main(String[] args) {
 		System.out.println("Gnight Tokki, Love you :)");
-
+		CharacterData npc1 = new CharacterData();
+		npc1.randomizeAge(18, 95);
+		System.out.println(npc1.getAge());
+		
+        CharacterData npc2 = new CharacterData(25, "Aragorn", "Human", "Male", "Noble", "Ranger", 
+                "Fighter", "Sword", "Brave", "Light");
+		System.out.println("NPC2 Name: " + npc2.getName());
+		System.out.println("NPC2 Age: " + npc2.getAge());
+		System.out.println("NPC2 Race: " + npc2.getRace());
+		
+        CharacterData npc3 = new CharacterData();
+        npc3.setName("Gimli");
+        npc3.setRace("Dwarf");
+        npc3.setWeapon("Axe");
+        System.out.println("NPC3 Name: " + npc3.getName());
+        System.out.println("NPC3 Race: " + npc3.getRace());
+        System.out.println("NPC3 Weapon: " + npc3.getWeapon());
 	}
 
 }
